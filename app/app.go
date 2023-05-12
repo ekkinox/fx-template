c3a4e419fecc05d77cd3810b1f4bda1d50dae404
Jonathan Vuillemin <ekkinox@gmail.com>
2023-05-12 11:57:05 +0200

    feat: wip

--- app/app.go
+++ app/app.go
@@ -1,11 +1,19 @@
 package app
 
 import (
+	"github.com/ekkinox/fx-template/modules/fxlogger"
+
 	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
 )
 
-var AppModule = fx.Module(
-	"app",
+var App = fx.New(
+	// resources
+	RegisterModules(),
 	RegisterHandlers(),
 	RegisterServices(),
+	// fx logger
+	fx.WithLogger(func(log *fxlogger.Logger) fxevent.Logger {
+		return log
+	}),
 )
--- main.go
+++ main.go
@@ -1,28 +1,7 @@
 package main
 
-import (
-	"github.com/ekkinox/fx-template/app"
-	"github.com/ekkinox/fx-template/modules/fxconfig"
-	"github.com/ekkinox/fx-template/modules/fxhttpserver"
-	"github.com/ekkinox/fx-template/modules/fxlogger"
-	"github.com/ekkinox/fx-template/modules/fxtracer"
-	"go.uber.org/fx/fxevent"
-
-	"go.uber.org/fx"
-)
+import "github.com/ekkinox/fx-template/app"
 
 func main() {
-	fx.New(
-		// core modules
-		fxconfig.FxConfigModule,
-		fxlogger.FxLoggerModule,
-		fxtracer.FxTracerModule,
-		fxhttpserver.FxHttpServerModule,
-		// app module
-		app.AppModule,
-		// logger
-		fx.WithLogger(func(log *fxlogger.Logger) fxevent.Logger {
-			return log
-		}),
-	).Run()
+	app.App.Run()
 }
