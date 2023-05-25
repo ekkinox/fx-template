7f88d458b49299825796405c394b31b44803f16c
Jonathan Vuillemin <ekkinox@gmail.com>
2023-05-25 09:42:34 +0200

    feat: posts crud

--- app/model/post.go
+++ app/model/post.go
@@ -1,9 +1,15 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Post struct {
-	gorm.Model
-	Title       string `json:"title" form:"title"`
-	Description string `json:"description" form:"description"`
+	ID          uint           `gorm:"primarykey" json:"id" form:"id"`
+	CreatedAt   time.Time      `json:"created_at" form:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at" form:"updated_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at" form:"deleted_at"`
+	Title       string         `json:"title" form:"title"`
+	Description string         `json:"description" form:"description"`
 }
